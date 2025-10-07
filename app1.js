7cc0df9e73b4181a1168e2c4399de2f4d063e9bf
Tachobo <wilmerardila2001@gmail.com>
2025-10-06 22:12:53 -0500

    Primer commit: primer ejercicio

---
+++ app1.js
@@ -0,0 +1,17 @@
+alert(`En este programa se le dara acceso a un usuario
+siempre y cuando cumpla con las siguientes condiciones
+usuario:admin
+contraseña:1234
+en caso contrario se le denegara el acceso`);
+//Asignamos y pedimos variables
+let usuario = prompt(`Por favor digite su nombre de usuario`);
+let contraseña = parseInt(prompt(`Por favor digite su contraseña`));
+//programamos las condicionales
+//se pone en comillas admin, para que se sepa que se
+//trabaja con una cadena de texto, si no lo hago, es como si trabajara
+//con una variable no definida, y no me daria respuesta
+if (usuario === "admin" && contraseña === 1234){
+    alert("Acceso permitido")
+}else{
+    alert("Acceso denegado")
+}
